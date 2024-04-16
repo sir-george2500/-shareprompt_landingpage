95ed16196bc7cf9b424af723a3d5c800df88a96a
George S. Mulbah <gsmulbah2500@gmail.com>
2024-04-16 21:34:49 +0000

    setup the background

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css';
+//importing Component
+import Nav from '../components/Nav.jsx';
+export const metadata = {
+  title: 'promptspace',
+  description: 'Discover and share Powereful AI prompt',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+      <Nav/>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+      <main className='app'>
+        {children}
+      </main>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+
+const Home = () => {
+  return (
+   <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">Find Out What new ? In
+    <br className="max-md:hidden"/>
+    <span className="orange_gradient text-center"> Share Prompt</span>
+    </h1>
+    <p className="desc text-center">
+      Prompt-Space is an open source tool for modern
+      world to create , and share powerful prompts
+    </p>
+   </section>
+  )
+}
+
+export default Home
