c27123791bdea28862710e45b849e26654caea3c
George S. Mulbah <gsmulbah2500@gmail.com>
2024-04-17 09:35:34 +0000

    created the about us card

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import CreatorCard from '@/components/CreatorCard'
-import React from 'react'
-
-export default function About() {
-  return (
-  <CreatorCard name="George S Mulbah" imageUrl={require('../../public/assets/images/george_mulbah.png')} role='Software Engineer' />
-  )
-}
-
