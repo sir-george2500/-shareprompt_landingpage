e4b33378432d8f3a98231cff0a90dd7d842590a2
George S. Mulbah <gsmulbah2500@gmail.com>
2024-04-17 10:01:20 +0000

    solve the image element problem

--- components/CreatorCard.tsx
+++ components/CreatorCard.tsx
@@ -11,7 +11,7 @@ const CreatorCard: React.FC<CreatorCardProps> = ({ imageUrl, name, role }) => {
   return (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <div className="flex flex-col items-center pb-10">
-        <Image className="mb-3 rounded-full mt-2 shadow-lg" src={imageUrl} alt={name} width={96} height={96} />
+        <Image className="mb-3 rounded-full mt-2 shadow-lg" src={imageUrl} alt="creator image"  width={96} height={96} />
         <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name}</h5>
         <span className="text-sm text-gray-500 dark:text-gray-400">{role}</span>
       </div>
