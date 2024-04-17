2b162f334ba369673777d79b513f6cffb403a9cd
George S. Mulbah <gsmulbah2500@gmail.com>
2024-04-17 00:54:37 +0000

    created image card successfully

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
+import PhotoCard from '@/components/PhotoCard';
 import React from 'react';
 
 const Home: React.FC = () => {
-  return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">Find Out What's New? In
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> Share Prompt</span>
-      </h1>
-      <p className="desc text-center">
-        Prompt-Space is an open-source tool for the modern world to create and share powerful prompts.
-      </p>
-    </section>
-  );
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">Find Out What's New? In
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center"> Share Prompt</span>
+            </h1>
+
+            <div className="grid grid-cols-2 gap-4">
+                <PhotoCard title='You can now edit  prompt' description='It now possible to edit your prompt with just a few clicks from your profile screen'  imageUrl='/assets/images/edit_prompt.png'/>
+                <PhotoCard title='You can Search for Prompt' description='You can search for related prompt based on a topic query or tag'  imageUrl='/assets/images/searchforprompt.png'/>
+                <PhotoCard title='Creating is now easier' description='We have made creating a prompt easier than before'  imageUrl='/assets/images/create_prompt.png'/>
+                <PhotoCard title='View other prompt' description='We have added a copy button to not just view but also support easy copying of others prompt'  imageUrl='/assets/images/view_other_user_prompt.png'/>
+            </div>
+        </section>
+    );
 };
 
 export default Home;
---
+++ components/PhotoCard.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import React from 'react';
+
+
+interface PhotoCardProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
+
+
+const PhotoCard: React.FC<PhotoCardProps> = ({ title, description, imageUrl }) => {
+    return(
+        <div className="mt-2">
+            <a href="#" className="block max-w-lg p-6 flex flex-row  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <div>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
+                </div>
+                <div className='flex items-center ml-auto'>
+                    <Image src={imageUrl} alt="Edit Prompt" width={600} height={600} />
+                </div>
+            </a>
+        </div>
+
+    );
+}
+
+export default PhotoCard;
